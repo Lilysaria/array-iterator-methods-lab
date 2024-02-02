35a7bda41301b4088cb751288bd712af1fc9fe20
saria <bloomyday56@gmail.com>
2024-02-02 00:14:03 -0800

    Finished the array tasks

---
+++ js/app.js
@@ -0,0 +1,182 @@
+// https://git.generalassemb.ly/SEI-CC/sei-1-22-wc/blob/main/Unit_1/07-js-continued/7.4-array-iterator-methods.md
+//https://git.generalassemb.ly/SEI-CC/sei-1-22-wc/blob/main/Unit_1/07-js-continued/7.4.1-array-iterator-methods-lab.md
+/*things are clearing up in this lesson. I was able to get a feel for the logic needed. I think my weaknesses
+are with arrow functions and parameters. I wanted to preserve my very initial code so I can refer to them. I hope it
+isn't too messy. I'm trying to find the best way to organize comments, thinking it might be better to keep comments at the bottom of page
+*/
+const inventors = [
+  { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
+  { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
+  { first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 },
+  { first: 'Marie', last: 'Curie', year: 1867, passed: 1934 },
+  { first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 },
+  { first: 'Nicolaus', last: 'Copernicus', year: 1473, passed: 1543 },
+  { first: 'Max', last: 'Planck', year: 1858, passed: 1947 },
+  { first: 'Katherine', last: 'Blodgett', year: 1898, passed: 1979 },
+  { first: 'Ada', last: 'Lovelace', year: 1815, passed: 1852 },
+  { first: 'Sarah E.', last: 'Goode', year: 1855, passed: 1905 },
+  { first: 'Lise', last: 'Meitner', year: 1878, passed: 1968 },
+  { first: 'Hanna', last: 'Hammarström', year: 1829, passed: 1909 }
+];
+// 1.
+const fifteenInvent = inventors.filter(inventor => inventor.year >= 1500 && inventor.year < 1600);
+console.log(fifteenInvent);
+// 2. 
+//const mapInventor = inventors.map(inventor)
+//This is an object literal being returned by the arrow function for each inventor.
+const inventorNames = inventors.map(inventor => ({ first: inventor.first, last: inventor.last }));
+console.log(inventorNames);
+// 3.
+//const inventorSort = inventors.sort(inventor => inventor.year) //this is what I wrote first
+const sortedInventors = inventors.sort((a, b) => a.year - b.year);
+console.log(sortedInventors)
+//the arrow function seems to take care of the if and else statements
+//it is like saying if a.year is less than b.year and then the sort method sorts it automatically in accending order
+// 4.
+// Array.prototype.find()
+// 4. Find the inventor object with the first name of 'Ada'
+const findAda = inventors.find(inventor => inventor.first === 'Ada')
+console.log(findAda)
+// 5.// Array.prototype.reduce()
+// 5. How many years did all the inventors live?
+//using the object, calculate each of the age they lived up to
+const howManyYears = inventors.reduce((total, inventor) => total + (inventor.passed - inventor.year), 0);
+console.log(howManyYears)
+//const howMany = inventors.reduce()passed - year
+// I didn't put the values total and inventor as the arguements.
+/*
+// Array.prototype.filter()
+// 1. Filter the array of inventors into a new array containing only the inventors born 
+in the 1500's
+
+ 
+// Array.prototype.map()
+// 2. Map the array of the inventors into a new array containing objects with just the first 
+and last names as properties
+// Hint:  Return a new object literal from the callback (don't mutate the object being passed
+ in to map)
+ 
+ 
+ 
+// Array.prototype.sort()
+// 3. Sort the inventors by birth date (year property), in ascending order
+ 
+ 
+ 
+// Array.prototype.find()
+// 4. Find the inventor object with the first name of 'Ada'
+ 
+ 
+ 
+// Array.prototype.reduce()
+// 5. How many years did all the inventors live?
+ 
+*/
+
+
+const people = [
+  'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick', 'Beecher, Henry',
+  'Beethoven, Ludwig', 'Begin, Menachem', 'Belloc, Hilaire', 'Bellow, Saul',
+  'Benchley, Robert', 'Benenson, Peter', 'Ben-Gurion, David',
+  'Benjamin, Walter', 'Benn, Tony', 'Bennington, Chester', 'Benson, Leana',
+  'Bent, Silas', 'Bentsen, Lloyd', 'Berger, Ric', 'Bergman, Ingmar',
+  'Berio, Luciano', 'Berle, Milton', 'Berlin, Irving', 'Berne, Eric',
+  'Bernhard, Sandra', 'Berra, Yogi', 'Berry, Halle', 'Berry, Wendell',
+  'Bethea, Erin', 'Bevan, Aneurin', 'Bevel, Ken', 'Biden, Joseph',
+  'Bierce, Ambrose', 'Biko, Steve', 'Billings, Josh', 'Biondo, Frank',
+  'Birrell, Augustine', 'Black, Elk', 'Blair, Robert', 'Blair, Tony',
+  'Blake, William'
+];
+// 6.
+//people.map(first,last) => people.first people.last
+//I forgot about using template literals
+const formattedNames = people.map(name => {
+  const [last, first] = name.split(', ');
+  return `${first} ${last}`;
+});
+console.log(formattedNames)
+/*
+ 
+*/
+
+
+const data = [
+  'car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van',
+  'bike', 'walk', 'car', 'van', 'car', 'truck'
+];
+
+/*
+ Array.prototype.reduce()
+7. Count the number of instances for each of the data items. The reduce should return an
+ object where the keys are 'car', 'truck', etc. and the values are the count.
+ Hint: Since you want to return an object, be sure to pass an empty {} for the initial
+ value of the "accumulator".
+*/
+// 7.
+/*
+data.reduce {} 
+return count {
+ car:
+ truck:
+ bike:
+ walk:
+ van:
+}
+*/
+const dataCount = data.reduce((acc, item) => {
+  acc[item] = (acc[item] || 0) + 1;
+  return acc;
+}, {});
+console.log(dataCount)
+
+const devs = [
+  { name: 'Wes', year: 1988 },
+  { name: 'Kait', year: 1986 },
+  { name: 'Irv', year: 1970 },
+  { name: 'Lux', year: 2015 }
+];
+/*
+Array.prototype.some()
+8. Check if at least one person is 19 or older?
+ Hint: To get today's year, use the getFullYear method of new Date(), i.e., 
+ new Date().getFullYear()
+Array.prototype.every()
+logic: 
+const 19Orolder= new Date(getFullYear) - devs.year
+if (19OrOlder >= 19)  
+return (19orOlder)
+const 19OrOlder =  devs.some()
+*/
+const currentYear = new Date().getFullYear();
+const isAtLeast19 = devs.some(devs => currentYear - devs.year >= 19);
+console.log(isAtLeast19)
+
+// 9.
+const currentYear2 = new Date().getFullYear();
+const isEvery19 = devs.every(devs => currentYear2 - devs.year >= 19);
+console.log(isEvery19)
+
+const comments = [
+  { text: 'Love this!', id: 523423 },
+  { text: 'Super good', id: 823423 },
+  { text: 'You are the best', id: 2039842 },
+  { text: 'Ramen is my fav food ever', id: 123523 },
+  { text: 'Nice Nice Nice!', id: 542328 }
+];
+
+// Array.prototype.find()
+// 10. Find the comment with the id of 823423
+/* const checkId = comments.id.find(checkId);
+function checkId(age) {
+ return age === 823423;
+}*/
+const foundComment = comments.find(function (comment) {
+  return comment.id === 823423;
+});
+console.log(foundComment)
+
+// Array.prototype.findIndex()
+// 11. Find the index of the comment with an id of 123523
+//const index = comment.findIndex.text('id')
+const indexComment = comments.findIndex(comment => comment.id === 123523);
+console.log(indexComment)
